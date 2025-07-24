85e7ba4f64ec7a2e605576baaa2d1c02b792a83f
Khush <khushdeep509.be22@chitkara.edu.in>
2025-07-24 14:52:34 +0530

    Updated port

--- bmi-app/vite.config.js
+++ bmi-app/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173, // optional
+    host: true,
+    allowedHosts: ['bmi-tracker-pk2b.onrender.com']
+  }
 })
